8fdf533e2333f26a3f204389352601577d89add8
Naseef Ali <naseefali@icloud.com>
2019-05-10 15:45:20 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+let sqlConfig = {
+
+    user: 'sa',
+    password: 'XXXXX',
+    server: 'XXXXXX',
+    database: 'XXXXX'
+};
+
+module.exports = {
+    secretKey: "XXXXXXXXX",
+    algorithm: 'HS256', //default: HS256
+    sqlConfig
+};
